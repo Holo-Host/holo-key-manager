7da4f90282620e5d07e5233f1ef326bd653d91e0
Dawid Urbas <dawid@getbold.agency>
2024-01-30 16:39:14 +0000

    Fix script and ignore

--- holo-key-manager-js-client/prebuild.cjs
+++ holo-key-manager-js-client/prebuild.cjs
@@ -5,7 +5,7 @@ require('dotenv').config({ path: '../.env' });
 const fs = require('fs');
 const path = require('path');
 
-const configContent = `
+const generateConfigContent = () => `
 export const browserConfig = {
     ChromeID: '${process.env.CHROME_ID}',
     FirefoxID: '${process.env.FF_ID}',
@@ -14,4 +14,15 @@ export const browserConfig = {
 `;
 
 const configFile = path.resolve(__dirname, 'src', 'browserConfig.ts');
-fs.writeFileSync(configFile, configContent, 'utf8');
+
+const writeConfigFile = (filePath, content) => {
+	if (fs.existsSync(filePath)) {
+		console.log('browserConfig exists, overriding...');
+	}
+	fs.writeFile(filePath, content, 'utf8', (err) => {
+		if (err) throw err;
+		console.log('browserConfig.ts has been saved!');
+	});
+};
+
+writeConfigFile(configFile, generateConfigContent());
