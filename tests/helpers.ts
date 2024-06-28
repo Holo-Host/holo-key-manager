32bada236f61bd6b09418c4df5a8fd87f678397c
Dawid Urbas <dawid@getbold.agency>
2024-06-28 11:15:02 +0200

    Finish first e2e test

--- tests/helpers.ts
+++ tests/helpers.ts
@@ -1,3 +1,4 @@
+import { access, constants } from 'fs';
 import { Browser, ElementHandle, launch, Page } from 'puppeteer';
 
 export const launchBrowserWithExtension = async (extensionPath: string): Promise<Browser> => {
@@ -13,21 +14,43 @@ export const openExtensionPage = async (browser: Browser, extensionId: string): 
 	return page;
 };
 
-export const clickButtonAndWaitForNewPage = async (
-	browser: Browser,
-	button: ElementHandle<Element>
-): Promise<Page> => {
-	await button?.click();
-
-	const newPagePromise = new Promise<Page | null>((resolve) =>
-		browser.once('targetcreated', async (target) => resolve(await target.page()))
+const waitForNewPage = (browser: Browser): Promise<Page> =>
+	new Promise((resolve, reject) =>
+		browser.once('targetcreated', async (target) => {
+			const page = await target.page();
+			page ? resolve(page) : reject(new Error('Failed to create new page'));
+		})
 	);
 
-	const newPage = await newPagePromise;
-	if (!newPage) {
-		throw new Error('Failed to create new page');
-	}
+export const clickButtonAndWaitForNewPage = async (
+	browser: Browser,
+	button: ElementHandle<Element>,
+	downloadPath: string
+): Promise<Page> => {
+	await button?.click();
+	const newPage = await waitForNewPage(browser);
 	await newPage.waitForNavigation();
 
+	const session = await newPage.createCDPSession();
+	await session.send('Page.setDownloadBehavior', {
+		behavior: 'allow',
+		downloadPath: downloadPath
+	});
+
 	return newPage;
+};
+
+export const fileExists = (filePath: string): Promise<boolean> => {
+	return new Promise((resolve) => {
+		const checkFile = () => {
+			access(filePath, constants.F_OK, (err) => {
+				if (!err) {
+					resolve(true);
+				} else {
+					setTimeout(checkFile, 100);
+				}
+			});
+		};
+		checkFile();
+	});
 };
