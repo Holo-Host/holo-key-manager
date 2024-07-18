e44c8a11d94f1f4b32f5c3756162b6bd4083c11f
Dawid Urbas <dawid@getbold.agency>
2024-07-18 09:17:37 +0200

    Cache to check test

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,15 @@
 import path from 'path';
 import { defineConfig } from 'vitest/config';
-
 export default defineConfig({
 	test: {
 		globals: true,
 		environment: 'node',
-		include: ['tests/**/*.test.ts']
+		include: ['tests/**/*.test.ts'],
+		poolOptions: {
+			threads: {
+				singleThread: true
+			}
+		}
 	},
 	resolve: {
 		alias: {
